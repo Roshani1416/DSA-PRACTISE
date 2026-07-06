6a796679bf1a56d8b6f688416404511258e8756a
Roshani1416 <roshanisingh7900@gmail.com>
2026-07-06 12:42:15 +0530

    Add solution: 1046. Last Stone Weight (Easy)

---
+++ Easy/1046-last-stone-weight/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a,b)-> b-a //max heap
+        );
+
+        for(int stone:stones){
+            pq.add(stone);
+        }
+
+        while(pq.size()>1){
+
+            int first=pq.poll();
+            int second = pq.poll();
+
+            if(first!=second){
+                pq.add(first-second);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.peek();
+        
+    }
+
+    
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# My LeetCode Solutions
+
+Auto-synced by leetcode-github-agent.
+
+<!-- LEETCODE-TABLE-START -->
+| # | Problem | Difficulty | Language | Solution |
+|---|---------|------------|----------|----------|
+| 1046 | [Last Stone Weight](https://leetcode.com/problems/last-stone-weight/) | Easy | java | [Code](Easy\1046-last-stone-weight) |
+<!-- LEETCODE-TABLE-END -->
